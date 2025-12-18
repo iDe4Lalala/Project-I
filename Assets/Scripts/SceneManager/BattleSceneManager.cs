31bc515678d0dcd51c55346356e35b68e3dff7db
iDe4Lalala <kouide27@gmail.com>
2025-12-18 14:37:57 +0900

    fix: タイマーの処理変更

--- Assets/Scripts/SceneManager/BattleSceneManager.cs
+++ Assets/Scripts/SceneManager/BattleSceneManager.cs
@@ -1,7 +1,6 @@
-using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-using TMPro;
+using System;
 using UnityEngine.Events;
 
 public class BattleSceneManager : MonoBehaviour
@@ -13,26 +12,28 @@ public class BattleSceneManager : MonoBehaviour
     [SerializeField] private HumanDataBase _playerDataBase;   // 人のデータベースリスト
     [field: SerializeField] public GameObject RiflePrefab { get; private set; }  // ライフル
     [SerializeField] private Transform _playerRespawnPoint;
-    [SerializeField] private TMP_Text _beforeBattleTimerText;   // 戦闘前のタイマーのテキスト
-    [SerializeField] private TMP_Text _battleStartedText;   // 戦闘開始のテキスト
-    [SerializeField] private float _beforeBattleTimer;   // 戦闘前のタイマーの時間
-    [SerializeField] private float _startedTextDisplayTime;   // 戦闘開始テキストの表示時間
     [SerializeField] private string _nextSceneName;   // 結果シーンの名前
     [SerializeField] private Canvas _battleCanvas;
+    [SerializeField] private float _battleTimer;   // 戦闘時間
     [field: SerializeField] public PlayerUIManager PlayerUIManager { get; private set; }
-    [field: SerializeField] public UnityEvent<bool> OnKilled { get; private set; }   // キルが発生した時のイベント
-    
-    public bool IsBeforeBattle { get; private set; }    // 戦闘前かどうか
+    [SerializeField] private BattleUIManager _battleUIManager;
+    [field: SerializeField] public UnityEvent<bool> OnKilled { get; private set; } // キルが発生した時のイベント
+    public event Action<float> OnTimerUpdated;   // タイマーが更新された時のイベント
+
+    private bool _isBattle;    // 戦闘中かどうか
     private PlayerComponents _playerComponents;
 
     private void OnEnable()
     {
         // 戦闘前の初期化
-        IsBeforeBattle = true;
-        _battleStartedText.gameObject.SetActive(false);
-        _beforeBattleTimerText.gameObject.SetActive(true);
-
+        _isBattle = false;
+        _battleUIManager.OnStartBattle += OnStartBattle;
         GeneratePlayer();
+    }
+
+    private void OnDisable()
+    {
+        _battleUIManager.OnStartBattle -= OnStartBattle;
     }
 
     private void GeneratePlayer()
@@ -64,6 +65,33 @@ public class BattleSceneManager : MonoBehaviour
         return;
     }
 
+    private void Update()
+    {
+        if(!_isBattle) return;
+
+        // タイマーを更新
+        if (_battleTimer >= 0)
+        {
+            _battleTimer -= Time.deltaTime;
+            OnTimerUpdated?.Invoke(_battleTimer);
+        }
+        else
+        {
+            // シーン遷移
+            _isBattle = false;
+            LoadOtherScene();
+        }
+    }
+
+    public void OnStartBattle(bool isStart)
+    {
+        /// <summary>
+        /// 戦闘開始時の処理
+        /// </summary>
+        
+        _isBattle = isStart;
+    }
+
     private void OnPlayerDied(GameObject player)
     {
         /// <summary>
@@ -75,23 +103,6 @@ public class BattleSceneManager : MonoBehaviour
         GeneratePlayer();
     }
 
-    private void Update()
-    {
-        // 戦闘前のタイマーを更新
-        if (IsBeforeBattle)
-        {
-            _beforeBattleTimer -= Time.deltaTime;
-            _beforeBattleTimerText.text = Mathf.CeilToInt(_beforeBattleTimer).ToString();
-
-            if (_beforeBattleTimer <= 0)
-            {
-                IsBeforeBattle = false;
-                _beforeBattleTimerText.gameObject.SetActive(false);
-                StartCoroutine(ShowBattleStartedText());
-            }
-        }
-    }
-
     public void LoadOtherScene()
     {
         /// <summary>
@@ -99,16 +110,5 @@ public class BattleSceneManager : MonoBehaviour
         /// </summary>
         
         SceneManager.LoadScene(_nextSceneName);
-    }
-
-    private IEnumerator ShowBattleStartedText()
-    {
-        /// <summary>
-        /// 戦闘開始のテキストのアニメーション
-        /// </summary>
-        
-        _battleStartedText.gameObject.SetActive(true);
-        yield return new WaitForSeconds(_startedTextDisplayTime);
-        _battleStartedText.gameObject.SetActive(false);
     }
 }
--- Assets/Scripts/UI/BattleUIManager.cs
+++ Assets/Scripts/UI/BattleUIManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using UnityEngine;
 using TMPro;
+using System;
 
 public class BattleUIManager : MonoBehaviour
 {
@@ -9,34 +10,43 @@ public class BattleUIManager : MonoBehaviour
     /// </summary>
     
     [SerializeField] private TMP_Text _battleTimerText;     // 残り戦闘時間のテキスト
-    [SerializeField] private float _battleTimer;   // 戦闘時間
     [SerializeField] private TMP_Text _killText;    // kill時テキスト
     [SerializeField] private float _killTextDisplayTime;   // killテキスト表示時間
     [SerializeField] private string _playerKillSentence;   // プレイヤーがキルした時の文章
     [SerializeField] private string _enemyKillSentence;    // 敵がキルした時の文章
+    [SerializeField] private TMP_Text _beforeBattleTimerText;   // 戦闘前のタイマーのテキスト
+    [SerializeField] private TMP_Text _battleStartedText;   // 戦闘開始のテキスト
+    [SerializeField] private float _beforeBattleTimer;   // 戦闘前の待ち時間
+    [SerializeField] private float _startedTextDisplayTime;   // 戦闘開始テキストの表示時間
     [SerializeField] private BattleSceneManager _battleSceneManager;
+    public event Action<bool> OnStartBattle;    // 戦闘開始時のイベント
 
     private void OnEnable()
     {
+        // イベント登録
+        _battleSceneManager.OnTimerUpdated += SetBattleTimer;
+
         // killテキストを非表示
         _killText.gameObject.SetActive(false);
+        _battleStartedText.gameObject.SetActive(false);
+        _beforeBattleTimerText.gameObject.SetActive(true);
+
+        StartCoroutine(ShowBeforeBattleTimerText());
     }
 
-    private void Update()
+    private void OnDisable()
     {
-        if(!_battleSceneManager.IsBeforeBattle) return;
+        // イベント解除
+        _battleSceneManager.OnTimerUpdated -= SetBattleTimer;
+    }
 
-        // タイマーを更新
-        if (_battleTimer >= 0)
-        {
-            _battleTimer -= Time.deltaTime;
-            _battleTimerText.text = _battleTimer.ToString("f2");
-        }
-        else
-        {
-            // シーン遷移
-            _battleSceneManager.LoadOtherScene();
-        }
+    public void SetBattleTimer(float time)
+    {
+        /// <summary>
+        /// 戦闘タイマーを設定する
+        /// </summary>
+        
+        _battleTimerText.text = time.ToString("f2");
     }
 
     public void OnKill(bool isPlayerKill)
@@ -66,5 +76,36 @@ public class BattleUIManager : MonoBehaviour
         _killText.gameObject.SetActive(true);
         yield return new WaitForSeconds(_killTextDisplayTime);
         _killText.gameObject.SetActive(false);
+    }
+
+    private IEnumerator ShowBeforeBattleTimerText()
+    {
+        /// <summary>
+        /// 戦闘前タイマーのアニメーション
+        /// </summary>
+
+        WaitForSeconds waitForSeconds = new (1f);
+        _beforeBattleTimerText.gameObject.SetActive(true);
+
+        for (int i = Mathf.CeilToInt(_beforeBattleTimer); i >= 1; i--)
+        {
+            _beforeBattleTimerText.text = i.ToString();
+            yield return waitForSeconds;
+        }
+
+        _beforeBattleTimerText.gameObject.SetActive(false);
+        OnStartBattle?.Invoke(true);
+        StartCoroutine(ShowBattleStartedText());
+    }
+
+    private IEnumerator ShowBattleStartedText()
+    {
+        /// <summary>
+        /// 戦闘開始のテキストのアニメーション
+        /// </summary>
+        
+        _battleStartedText.gameObject.SetActive(true);
+        yield return new WaitForSeconds(_startedTextDisplayTime);
+        _battleStartedText.gameObject.SetActive(false);
     }
 }
